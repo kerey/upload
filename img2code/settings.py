1a592d5739d0ee95ddc97cf6de5fbddd49d9ac9a
Akzholtay Dagzhanuly <akzholtay.k@gmail.com>
2020-05-17 16:13:45 +0600

    feat: django rest framework

--- img2code/settings.py
+++ img2code/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'upload'
 ]
 
@@ -119,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
