8880a8886ae4529d17c2ca3be675dec3cd887528
Akzholtay Dagzhanuly <akzholtay.k@gmail.com>
2020-05-19 22:14:22 +0600

    content length header

--- img2code/settings.py
+++ img2code/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware'
 ]
 
 ROOT_URLCONF = 'img2code.urls'
