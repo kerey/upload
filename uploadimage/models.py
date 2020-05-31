eafa75ffa524b6214f88dae39c6b61c5e8d86d0b
Akzholtay Dagzhanuly <akzholtay.k@gmail.com>
2020-05-31 12:45:30 +0600

    fix: comments

--- uploadimage/models.py
+++ uploadimage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class File(models.Model):
+    # all images from front will be saved in directory static/images
     file = models.FileField(upload_to='static/images', blank=False, null=False)
 
     def __str__(self):
--- uploadimage/views.py
+++ uploadimage/views.py
@@ -13,13 +13,19 @@ class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
 
     def post(self, request, *args, **kwargs):
+        # this uploads file
         file_serializer = FileSerializer(data=request.data)
-
+        # this is file validation
         if file_serializer.is_valid():
+            # save file in directory static/images
             file_serializer.save()
+            # test function generates gui file and saves it in directory static/code
+            # then returns path for that gui file
             gui_file_path = test(file_serializer.data['file'])
+            # compile function gets gui file path, converts it to html and returns entire html
             html = compile(gui_file_path)
             print(html)
+            # finally, return html as response
             zip_file = open(html, 'rb')
             response = HttpResponse(FileWrapper(zip_file), content_type='application/zip')
             response['Content-Disposition'] = 'attachment; filename="%s"' % 'CDX_COMPOSITES_20140626.zip'
